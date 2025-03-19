d15ee8121f3393c74884ea91482be074fc9f6efc
WindowsOverUbuntu <31lussield@jamestownpublicschools.org>
2025-03-19 11:21:11 -0400

    undertale trivia!

---
+++ trivia_undertale.py
@@ -0,0 +1,8 @@
+print("Trivia time!!!")
+print("What does the stick sell for at the TEM shop?")
+
+userinput = input()
+if userinput in ["150 gold", "150 G", "150 g" ,"150"]:
+  print("Correct!!!")
+else:
+  print("INCORRECT!!!")
