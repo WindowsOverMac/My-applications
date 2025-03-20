bf1ba955c43ffac0a5c60107f35c81e7ca32e67d
WindowsOverUbuntu <31lussield@jamestownpublicschools.org>
2025-03-20 11:35:07 -0400

    trivia

---
+++ changedfunfact.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+  string response;
+    cout << "Want to hear a fun fact?";
+    std::cin >> response;
+     transform(response.begin(), response.end(), response.begin(), ::tolower);
+    if (response =="yes"||response == "Yes"||response =="ye"||response == "Ye"||response == "sure"||response =="Sure"||response =="y"||response =="Y"){
+    cout << "White latex and dark latex are naturally enemies"<< endl;
+    } else{
+      cout << "Okay, no fun fact told." << endl;
+   } return 0;
+}
