64bd83973277d53e2c8958bb3ddea16eef504199
WindowsOverUbuntu <31lussield@jamestownpublicschools.org>
2025-03-20 11:03:44 -0400

    changed trivia!!1!

---
+++ trivia_Changed.py
@@ -0,0 +1,8 @@
+print("Trivia time!!!")
+print("What BIOLOGICAL gender is 'puro'")
+userinput = input()
+
+if userinput in["gender neutral","neutral","nonbinary","Gender neutral","Neutral","Nonbinary","Gender Neutral", "Non binary","Non Binary","NonBinary","GENDER NEUTRAL","NEUTRAL","NONBINARY","NON BINARY","Neither","NEITHER","neither"]:
+  print("Correct!")
+else:
+  print("INCORRECT!!!")
