f87598e8a2169f7bb0d0a0963ba959783c756374
WindowsOverUbuntu <31lussield@jamestownpublicschools.org>
2025-03-21 08:48:43 -0400

    rollthedie.py

--- rollthedie.py
+++ rollthedie.py
@@ -3,7 +3,7 @@ print("Roll the dice?")
 
 userinput = input()
 
-if  userinput in ["Yes","Yeah","Yup","Si","Sure","yes","yeah","si","sure","ye","Ye"]:
+if  userinput in ["Yes","Yeah","Yup","Si","Sure","yes","yeah","si","sure","ye","Ye","y","Y"]:
   random_int = random.randint(1, 50)
   print(random_int)
 else:
