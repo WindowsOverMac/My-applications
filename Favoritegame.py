bb74b99932152b1fd60e36c9d26c336f900acf92
WindowsOverUbuntu <31lussield@jamestownpublicschools.org>
2025-03-21 09:38:01 -0400

    Triva!!1!

---
+++ Favoritegame.py
@@ -0,0 +1,9 @@
+print("Guess my favorite game i have on my phone!!")
+
+userinput = input()
+
+if userinput in ["SFS","sfs","Space flight sim","space flight sim","Space Flight Sim","Space Flight Sim","SPACE FLIGHT SIM","Space flight simulator","space flight simulator","Space Flight Simulator","SPACE FLIGHT SIMULATOR"]:
+  print("Yes!!! correct!!")
+else:
+    print("WRONG!")
+  
